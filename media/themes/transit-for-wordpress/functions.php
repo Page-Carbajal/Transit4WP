937ebe78f002e6fef5b0f98f4a6617ca15e57fc0
awesomepage <Ceiba.72>
2015-03-13 17:24:52 -0600

    Clean up after a little confusion with branches :p

--- media/themes/transit-for-wordpress/functions.php
+++ media/themes/transit-for-wordpress/functions.php
@@ -8,26 +8,3 @@ $themeSetup = new Transit4WP\ThemeSetup();
 add_action( 'init', array( $themeSetup, 'init' ) );
 //Load Metadata into the post object
 add_action( 'the_post', array( 'Transit4WP\LoadMetadata', 'init' ) );
-
-
-add_filter( 'template_include', 'controllerPrototype' );
-function controllerPrototype( $template ){
-
-   $pathArray = explode( '/', $template );
-   $fileName = $pathArray[count($pathArray)-1];
-   switch( $fileName ){
-      case 'singles.php':
-
-         $data = Timber::get_context();
-         $data['posts'] = Timber::get_posts();
-         $data['isController'] = true;
-         Timber::render( 'single.twig', $data );
-
-         $template = false;
-         break;
-      default:
-         break;
-   }
-
-   return $template;
-}
--- media/themes/transit-for-wordpress/single.php
+++ media/themes/transit-for-wordpress/single.php
@@ -1,5 +1,1 @@
 <?php
-$data = Timber::get_context();
-$data['posts'] = Timber::get_posts();
-$data['var'] = 123;
-Timber::render( 'single.twig', $data );
