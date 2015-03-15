fef0bfe462dc3e6d65c192792a98c8d81919a7a9
awesomepage <Ceiba.72>
2015-03-14 23:52:07 -0600

    Theme Files now

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+require_once( 'includes/classes/Theme-Setup.class.php' );
+require_once( 'includes/classes/Theme-Options.class.php' );
+require_once( 'includes/classes/Load-Metadata.class.php' );
+
+//Setup Theme in an elegant way with fewer hooks called.
+$themeSetup = new Transit4WP\ThemeSetup();
+add_action( 'init', array( $themeSetup, 'init' ) );
+//Load Metadata into the post object
+add_action( 'the_post', array( 'Transit4WP\LoadMetadata', 'init' ) );
